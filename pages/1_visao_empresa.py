f2fa492e29c7c308510f9f80e94b189303786b28
Leandro de Almeida Fernandes <151480602+LeandroAlferDs@users.noreply.github.com>
2023-12-22 21:37:37 -0300

    Update 1_visao_empresa.py

--- pages/1_visao_empresa.py
+++ pages/1_visao_empresa.py
@@ -128,7 +128,7 @@ def clean_code( df1 ):
     return df1
 #-----------------------------Início da Estrutura lógica do código-------------------------
 #import dataset
-pd.read_csv('programacao/train.csv')
+df= pd.read_csv(programacao/train.csv)
 
 #Limpando os dados
 df1 = clean_code (df)
